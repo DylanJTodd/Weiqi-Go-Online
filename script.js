a5a9a65dc93053b7a6e0dd346666193479a4b90c
Dylan <dylan.todd04@gmail.com>
2023-12-28 18:58:14 -0500

    started liberty checking, and score calculating.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ class Board
 {
     constructor(canvas, ctx, img) 
     {
+        this.goldScore = 0;
+        this.whiteScore = 0;
         this.canvas = canvas;
         this.ctx = ctx;
         this.img = img;
@@ -11,23 +13,131 @@ class Board
         this.currentTurn = -1;
     }
 
-    validCheck(x, y) 
+    clone()
     {
-        return this.stones[x][y] == 0;
+        let newBoard = new Board(this.canvas, this.ctx, this.img)
+        newBoard.cellSize = this.cellSize;
+        newBoard.currentTurn = this.currentTurn;
+        newBoard.stoneGroups = this.stoneGroups.map(group => group.clone(newBoard));
+        newBoard.stones = this.stones.map(row => row.map(cell => {
+            if (cell instanceof Stone){
+                return cell.clone(this, cell.group);
+            }
+            else{
+                return cell;
+            }
+        }))
+        return newBoard;
+    }
+
+    validCheck(stone) 
+    {
+        if (this.stones[stone.y][stone.x] !== 0) {
+            return false;
+        }
+
+        let newBoard = this.clone();
+        let newStone = stone.clone();
+
+        newBoard.stones[newStone.y][newStone.x] = newStone;
+        newBoard.checkAndMerge(newStone);
+        
+        let removedGroups = newBoard.libertyCheck();
+        if (removedGroups === null) {
+            return true;
+        }
+
+        let isPlaceable = false;
+        for (let group of removedGroups)
+        {
+            for (let removableStone of group.stones)
+            {
+
+                console.log("My Stone: " + newStone)
+                console.log("Removed Stone: " + removableStone)
+                if (removableStone.color !== newStone.color)
+                {
+                    isPlaceable = true;
+                }
+                newBoard.stones[removableStone.y][removableStone.x] = 0;
+            }
+            group.deleteGroup(newBoard);
+        }
+
+        if (newBoard.stones[newStone.y][newStone.x] !== newStone && !isPlaceable)
+        {
+            return false;
+        }
+
+        return true;
     }
 
     libertyCheck() 
     {
+        let noLiberties = [];
+
         for (let groups of this.stoneGroups)
         {
-
             if (groups.calculateLiberties() == 0)
             {
-                return groups;
+                noLiberties.push(groups);
             }
         }
-        return null;
+
+        if (noLiberties.length == 0)
+        {
+            return null;
+        }
+        else{
+            return noLiberties;
+        }
     }
+    checkAndMerge(stone) {
+        const directions = [
+            {dx: -1, dy: 0},
+            {dx: 1, dy: 0},
+            {dx: 0, dy: -1},
+            {dx: 0, dy: 1}
+        ];
+        let groupsToMerge = [];
+    
+        for (let direction of directions) {
+            let x = stone.x + direction.dx;
+            let y = stone.y + direction.dy;
+            if (y >= 0 && y < this.stones.length && x >= 0 && x < this.stones[y].length) {
+                let neighbor = this.stones[y][x];
+                if (neighbor && neighbor.color === stone.color) {
+                    groupsToMerge.push(neighbor.group);
+                }
+            }
+        }
+    
+        if (groupsToMerge.length > 0) {
+            this.merge(groupsToMerge, stone);
+        }
+        else {
+            var group = new Group(stone, this, stone.color);
+            stone.group = group;
+            this.stoneGroups.push(group);
+        }
+    }
+    
+    
+    merge(groups, stone) {
+        let newGroup = new Group(stone, this, stone.color);
+        stone.group = newGroup;
+        this.stoneGroups.push(newGroup);
+        for (let group of groups) {
+            for (let index of group.stones) {
+                newGroup.stones.add(index);
+                index.group = newGroup;
+            }
+            group.deleteGroup();
+        }
+        return newGroup;
+    }
+    
+    
 
     redraw() 
     {
@@ -36,10 +146,12 @@ class Board
         for(let i = 0; i < this.stones.length; i++) {
             for(let j = 0; j < this.stones[i].length; j++) {
                 if (this.stones[i][j] instanceof Stone) {
+                    let color = this.stones[i][j].color == 1 ? "White" : "Gold";
                     this.ctx.beginPath();
                     this.ctx.arc((j+1) * this.cellSize, (i+1) * this.cellSize, 20, 0, 2 * Math.PI, false);
-                    this.ctx.fillStyle = this.stones[i][j].color == 1 ? "White" : "Gold";
+                    this.ctx.fillStyle = color;
                     this.ctx.fill();
+
                 }
             }
         }
@@ -48,19 +160,27 @@ class Board
 
 class Group 
 {
-    constructor(stone, board) 
+    constructor(stone, board, color) 
     {
         this.stones = new Set([stone]);
         this.board = board;
+        this.color = color;
     }
 
-    deleteGroup(board)
+    clone(board)
     {
-        for (let index in board.stoneGroups)
+        let clonedStones = new Set(Array.from(this.stones).map(stone => stone.clone(board, this)));
+        let newGroup = new Group([...clonedStones][0], board, this.color);
+        return newGroup;
+    }
+
+    deleteGroup()
+    {
+        for (let index in this.board.stoneGroups)
         {
-            if (board.stoneGroups[index] === this)
+            if (this.board.stoneGroups[index] === this)
             {
-                board.stoneGroups.splice(index,1)
+                this.board.stoneGroups.splice(index,1)
             }
         }
     }
@@ -89,36 +209,31 @@ class Group
         }
         return liberty;
     }
-
-
-    merge(groups,stone) 
-    {
-        let newGroup = new Group(stone, this.board)
-        for (let group of groups)
-        {
-            for (let index of group)
-            {
-                newGroup.add(index)
-            }
-            group.deleteGroup(this.board)
-        }
-
-        return newGroup;
-    }
 }
 
 class Stone
 {
-    constructor(x,y,color, group, board)
+    constructor(x,y,color,board)
     {
         this.x = x;
         this.y = y;
         this.color = color;
         this.board = board;
-        this.group = group || new Group(this, board);
-        this.group.liberties = this.group.calculateLiberties();
+        this.group = null;
+    }
+
+    clone(board, group)
+    {
+        let newStone = new Stone(this.x, this.y, this.color, board);
+        newStone.group = group;
+        return newStone;
+    }
+
+    toString() {
+        return ("X:" + this.x + " Y:" + this.y + " Color: " + this.color);
     }
 }
+
 
 window.onload = function() 
 {
@@ -133,7 +248,6 @@ window.onload = function()
     img.onload = function() {
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
     }
-
     var board = new Board(canvas, ctx, img);
 
     canvas.onclick = function(e) 
@@ -141,30 +255,49 @@ window.onload = function()
         var x = (Math.round((e.pageX - canvas.offsetLeft) / board.cellSize))-1;
         var y = (Math.round((e.pageY - canvas.offsetTop) / board.cellSize))-1;
 
-        if (board.validCheck(y,x)) {
-            var color = board.currentTurn == -1 ? 1 : -1;
-            var stone = new Stone(x, y, color, null, board);
+        var color = board.currentTurn;
+        var stone = new Stone(x, y, color, board);
+
+        if (board.validCheck(stone)) {
             board.stones[y][x] = stone;
             board.currentTurn *= -1;
-            var group = new Group(stone, board);
-            board.stoneGroups.push(group);
-
+            board.checkAndMerge(stone);
+            
             let removedGroup = board.libertyCheck();
-            if (removedGroup !== null)
+            if (removedGroup != null)
             {
-                for (let removableStone of removedGroup.stones)
-                {
-                    board.stones[removableStone.x][removableStone.y] = 0;
-                }
-                removedGroup.deleteGroup(board);
-            }
-        }
-        else
-        {
-            stone = board.stones[y][x]; 
-            console.log("Color:" + stone.color + " X,Y:" + "(" + stone.x + " " + stone.y + ") " + "Liberties: " + stone.group.calculateLiberties());
-        }
+                let whiteCounter = 0;
+                let goldCounter = 0;
 
+                for (let removableGroup of removedGroup)
+                {
+                    if (removableGroup.color != stone.color)
+                    {
+                        for (let removableStone of removableGroup.stones)
+                        {
+                            board.stones[removableStone.y][removableStone.x] = 0;
+                            if (removableStone.color == 1)
+                            {
+                                goldCounter+= 1;
+                            }
+                            else
+                            {
+                                whiteCounter += 1;
+                            }
+                        }
+                        removableGroup.deleteGroup(board);
+                    }
+                }
+
+                board.whiteScore += whiteCounter;
+                board.goldScore += goldCounter;
+
+                document.getElementById('goldCounter').innerText = board.goldScore;
+                document.getElementById('whiteCounter').innerText = board.whiteScore;
+            }
+            board.stones[y][x] = stone;
+
+        }
         setTimeout(() => board.redraw(), 0);
     }
 }
